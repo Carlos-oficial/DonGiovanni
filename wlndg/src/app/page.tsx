10d10e1ea00b6d28d9ad864435c5bab98fe4a80d
Carlos-oficial <carlosribeiro2003oficial@gmail.com>
2023-10-05 17:03:40 +0100

    fix export

--- wlndg/src/app/page.tsx
+++ wlndg/src/app/page.tsx
@@ -5,18 +5,9 @@ import WLNDG from "./components/WLNDG";
 import LightSwitch from "./components/light_switch";
 
 
-export const DarkMode = createContext({ dark: false, toggle_dark: () => { } });
-
-
-
 export default function Home() {
-  const [dark, setDark] = useState(false);
-  const toggle_dark = () => {
-    setDark(!dark)
-  }
   return (
     <>
-      <DarkMode.Provider value={{ dark, toggle_dark }}>
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 dark:bg-slate-800`}>
 
           <div className="relative flex place-items-center ">
@@ -31,7 +22,6 @@ export default function Home() {
 
           </div>
         </main>
-      </DarkMode.Provider>
     </>
   )
 }
