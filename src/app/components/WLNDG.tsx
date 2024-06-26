320994a5379a07ca23398eec335c3bb0596b3352
Carlos <carlosribeiro2003oficial@gmail.com>
2024-06-26 12:15:01 +0100

    links and sidebar

--- src/app/components/WLNDG.tsx
+++ src/app/components/WLNDG.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 
 export default function WLNDG() {
     return <>
-
             <Image
                 className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert rotating"
                 src="/drawing lettering.png"
